6d1adfa8b88646da32e60df8a8f49a3d980e192f
Celyne Zarraga <crzarraga@up.edu.com>
2024-04-25 21:15:56 +0800

    chore: hide routes

--- backend/src/routes/api/users.ts
+++ backend/src/routes/api/users.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import * as controllers from "../../controllers/users";
-import authMiddleware from "../../middleware/authorization";
+// import authMiddleware from "../../middleware/authorization";
 
 const routes = Router();
 
 routes.route("/")
-  .get(authMiddleware, controllers.getAll)
+  // .get(authMiddleware, controllers.getAll)
   .post(controllers.signUp);
 
-routes.route("/:id")
-  .get(authMiddleware, controllers.getOne)
-  .patch(authMiddleware, controllers.updateOne)
-  .delete(authMiddleware, controllers.deleteOne);
+// routes.route("/:id")
+//   .get(authMiddleware, controllers.getOne)
+//   .patch(authMiddleware, controllers.updateOne)
+//   .delete(authMiddleware, controllers.deleteOne);
 
 routes.route("/login")
   .post(controllers.authenticate);
