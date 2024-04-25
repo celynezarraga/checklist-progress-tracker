a067159f71d380bb3613efc76c677fd3b127652c
Celyne Zarraga <crzarraga@up.edu.com>
2024-04-25 13:51:01 +0800

    Backend initial setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,50 @@
 import express, { Application, Request, Response } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
+import ErrorMiddleware from "./middleware/error";
 
 const PORT = 3030;
 
 const app: Application = express();
 
+app.use(express.json());
+
+// MIDDLEWARE
+app.use(morgan("common"));
+app.use(helmet());
+app.use(rateLimit({
+	windowMs: 30 * 60 * 1000, // 1- minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes).
+	standardHeaders: "draft-7",
+	legacyHeaders: false,
+  message: "Too many requests."
+}));
+app.use(ErrorMiddleware);
+
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({
-        message: "Hello world!!!"
-    });
+  res.json({
+    message: "Hello world!!!"
+  });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello World from post!!!",
+    data: req.body
+  });
+});
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    message: "Error."
+  });
 });
 
 app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server has started at port: ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server has started at port: ${PORT}`);
 });
 
 export default app;
---
+++ backend/src/middleware/error.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+import Error from "../interface/error";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ErrorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  const status = error.status ?? 500;
+  const message = error.message ?? "Internal Server Error";
+  res.status(status).json({status, message});
+};
+
+export default ErrorMiddleware;
